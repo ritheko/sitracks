f9ff96c2e0771587451846d98a887676d05e451a
ritheko <v.rizhko@gmail.com>
2024-11-30 15:22:20 +0200

    Add files via upload

--- js/stem-tracks.js
+++ js/stem-tracks.js
@@ -26,7 +26,7 @@ playlist
       waveOutlineColor: '#E0EFF1',
     },
     {
-      src: "https://www.inkid.org/All.mp3",
+      src: "https://www.inkid.org/ALL.mp3",
       name: "All",
       waveOutlineColor: '#E0EEF1',
     },
@@ -34,46 +34,46 @@ playlist
       src: "https://www.inkid.org/Bass.mp3",
       name: "Bass",
     },
-    {
-      src: "https://www.inkid.org/Cello.mp3",
-      name: "Cello",
-    },
-    {
-      src: "https://www.inkid.org/Drums.mp3",
-      name: "Drums",
-    },
-    {
-      src: "http://www.inkid.org/EG%20DJ.mp3",
-      name: "EG DJ",
-    },
-    {
-      src: "https://www.inkid.org/EG-T.mp3",
-      name: "EG T",
-    },
-    {
-      src: "https://www.inkid.org/Keys.mp3",
-      name: "Keys",
-    },
-    {
-      src: "https://www.inkid.org/Piano.mp3",
-      name: "Piano",
-    },
-    {
-      src: "https://www.inkid.org/Synth Bass.mp3",
-      name: "Synth Bass",
-    },
-    {
-      src: "http://www.inkid.org/Trumpet%201.mp3",
-      name: "Trumpet 1",
-    },
-    {
-      src: "http://www.inkid.org/Trumpet%202.mp3",
-      name: "Trumpet 2",
-    },
-    {
-      src: "http://www.inkid.org/Violin.mp3",
-      name: "Violin",
-    },
+    // {
+    //   src: "https://www.inkid.org/Cello.mp3",
+    //   name: "Cello",
+    // },
+    // {
+    //   src: "https://www.inkid.org/Drums.mp3",
+    //   name: "Drums",
+    // },
+    // {
+    //   src: "http://www.inkid.org/EG%20DJ.mp3",
+    //   name: "EG DJ",
+    // },
+    // {
+    //   src: "https://www.inkid.org/EG-T.mp3",
+    //   name: "EG T",
+    // },
+    // {
+    //   src: "https://www.inkid.org/Keys.mp3",
+    //   name: "Keys",
+    // },
+    // {
+    //   src: "https://www.inkid.org/Piano.mp3",
+    //   name: "Piano",
+    // },
+    // {
+    //   src: "https://www.inkid.org/Synth Bass.mp3",
+    //   name: "Synth Bass",
+    // },
+    // {
+    //   src: "http://www.inkid.org/Trumpet 201.mp3",
+    //   name: "Trumpet 1",
+    // },
+    // {
+    //   src: "http://www.inkid.org/Trumpet 202.mp3",
+    //   name: "Trumpet 2",
+    // },
+    // {
+    //   src: "http://www.inkid.org/Violin.mp3",
+    //   name: "Violin",
+    // },
   ])
   .then(function () {
     //can do stuff with the playlist.
